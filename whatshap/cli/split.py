92cc9c881b0e041986c863ab90bc556e3beb9e22
Peter Ebert <pebert@mpi-inf.mpg.de>
2019-10-23 18:33:43 +0200

    refactored initial processing of haplotag list file; add option to discard unknown reads

--- whatshap/cli/split.py
+++ whatshap/cli/split.py
@@ -9,7 +9,7 @@ import logging
 import sys
 import gzip
 import pysam
-from collections import defaultdict
+from collections import defaultdict, Counter
 from subprocess import Popen, PIPE
 
 from contextlib import ExitStack
@@ -37,6 +37,13 @@ def add_arguments(parser):
 	arg('--only-largest-block', default=False, action='store_true',
 		help='Only consider reads to be tagged if they belong to the largest '
 		'phased block (in terms of read count) on their respective chromosome')
+	arg('--discard-unknown-reads', default=False, action='store_true',
+		help='Only check the haplotype of reads listed in the haplotag list file. '
+			'Reads (read names) not contained in this file will be discarded. '
+			'In the default case (= keep unknown reads), those reads would be '
+			'considered untagged and end up in the respective output file. '
+			'Please be sure that the read names match between the input FASTQ/BAM '
+			'and the haplotag list file.')
 	arg('--read-lengths-histogram', default=None,
 		help='Output file to write read lengths histogram to in tab separated format.')
 	arg('reads_file', metavar='READS', help='Input FASTQ/BAM file with reads (fastq can be gzipped)')
@@ -103,12 +110,114 @@ def read_fastq(filename):
 		yield name, record
 
 
+def select_reads_in_largest_phased_blocks(block_sizes, block_to_readnames):
+	"""
+	:param block_sizes:
+	:param block_to_readnames:
+	:return:
+	"""
+	selected_reads = set()
+	logger.info('Determining largest blocks/phasesets per chromosome')
+	for chromosome, block_counts in block_sizes.items():
+		block_name, reads_in_block = block_counts.most_common(1)[0]
+		logger.info(
+			'Chromosome: {} - Phaseset: {} - Tagged reads: {}'.format(
+				chromosome, block_name, reads_in_block
+			)
+		)
+		selected_reads = selected_reads.union(set(block_to_readnames[(chromosome, block_name)]))
+	logger.info('Total number of haplo-tagged reads in all largest phased blocks: {}'.format(len(selected_reads)))
+	return selected_reads
+
+
+def process_haplotag_list_file(haplolist, line_parser, only_largest_blocks, discard_unknown_reads):
+	"""
+	:param haplolist:
+	:param line_parser:
+	:param only_largest_blocks:
+	:param discard_unknown_reads:
+	:return:
+	"""
+	# TODO: obviously this won't work for more than two haplotypes
+	haplotype_to_int = {'none': 0, 'H1': 1, 'H2': 2}
+
+	is_header = haplolist.readline().startswith('#')
+	if not is_header:
+		haplolist.seek(0)
+
+	# needed to determine largest phased block
+	block_sizes = defaultdict(Counter)
+	# for later removal of reads not in largest phased block;
+	# since this can grow quite a bit, only fill if needed
+	blocks_to_readnames = defaultdict(set)
+
+	# this set should not be too large given
+	# that the haplotag list file contains only
+	# a subset of the reads in the input FASTQ/BAM
+	known_reads = set()
+
+	readname_to_haplotype = defaultdict(int)
+	total_reads = 0
+
+	for line in haplolist:
+		readname, haplo_name, phaseset, chromosome = line_parser(line)
+		total_reads += 1
+		haplo_num = haplotype_to_int[haplo_name]
+		if haplo_num == 0:
+			if discard_unknown_reads:
+				known_reads.add(readname)
+			# Some "trickery" here:
+			# Haplotype 0 means untagged;
+			# the return value of a defaultdict(int)
+			# is zero for unknown keys, so no need to store
+			# anything unless "--discard-unknown-reads" is True,
+			# in which case we need to know all read names
+			continue
+		readname_to_haplotype[readname] = haplo_num
+		if only_largest_blocks:
+			block_sizes[chromosome][phaseset] += 1
+			blocks_to_readnames[(chromosome, phaseset)].add(readname)
+
+	tagged_reads = len(readname_to_haplotype)
+	untagged_reads = total_reads - tagged_reads
+	logger.info('Total number of reads in haplotag list: {}'.format(total_reads))
+	logger.info('Total number of haplo-tagged reads: {}'.format(tagged_reads))
+	logger.info('Total number of untagged reads: {}'.format(untagged_reads))
+
+	if discard_unknown_reads:
+		known_reads = known_reads.union(set(readname_to_haplotype.keys()))
+		num_known_reads = len(known_reads)
+		assert total_reads == num_known_reads, \
+			'Mismatch between total number of reads and known reads: {} vs {}'.format(total_reads, num_known_reads)
+
+	if only_largest_blocks:
+		selected_reads = select_reads_in_largest_phased_blocks(
+			block_sizes,
+			blocks_to_readnames
+		)
+		readname_to_haplotype = defaultdict(int, {k: readname_to_haplotype[k] for k in selected_reads})
+		num_removed_reads = total_reads - len(readname_to_haplotype)
+		logger.info('Number of reads removed / '
+					'reads not overlapping largest phased blocks: {}'.format(num_removed_reads))
+
+	return readname_to_haplotype, known_reads
+
+
+def _two_column_parser(line):
+	cols = line.strip().split('\t')[:2]
+	return cols[0], cols[1], None, None
+
+
+def _four_column_parser(line):
+	return line.strip().split('\t')[:4]
+
+
 def check_haplotag_list_information(haplotag_list, exit_stack):
 	"""
 	Check if the haplotag list file has at least 4 columns
 	(assumed to be read name, haplotype, phaseset, chromosome),
 	or at least 2 columns (as above). Fails if the haplotag file
-	is not tab-separated
+	is not tab-separated. Return suitable parser for format
 
 	:param haplotag_list: Tab-separated file with at least 2 or 4 columns
 	:param exit_stack:
@@ -121,15 +230,17 @@ def check_haplotag_list_information(haplotag_list, exit_stack):
 	has_chrom_info = False
 	try:
 		_, _, _, _ = first_line.split('\t')[:4]
+		line_parser = _four_column_parser
 	except ValueError:
 		try:
 			_, _ = first_line.split('\t')[:2]
+			line_parser = _two_column_parser
 		except ValueError:
 			raise ValueError('First line of haplotag list file does not have '
 							'at least 2 columns, or it is not tab-separated: {}'.format(first_line))
 	else:
 		has_chrom_info = True
-	return haplo_list, has_chrom_info
+	return haplo_list, has_chrom_info, line_parser
 
 
 def run_split(
@@ -141,19 +252,17 @@ def run_split(
 		add_untagged=False,
 		pigz=False,
 		only_largest_block=False,
+		discard_unknown_reads=False,
 		read_lengths_histogram=None,
 	):
 
 	timers = StageTimer()
 	timers.start('split-run')
 
-	# TODO: obviously this won't work for more than two haplotypes
-	haplotype_to_int = {'none': 0, 'H1': 1, 'H2': 2}
-
 	with ExitStack() as stack:
 		timers.start('split-init')
 
-		haplo_list, has_haplo_chrom_info = check_haplotag_list_information(list_file, stack)
+		haplo_list, has_haplo_chrom_info, line_parser = check_haplotag_list_information(list_file, stack)
 
 		if only_largest_block:
 			logger.debug('User selected "--only-largest-block", this requires chromosome '
@@ -163,53 +272,12 @@ def run_split(
 								'information, which is required to select only reads from the '
 								'largest phased block. Columns 3 and 4 are missing.')
 
-		largest_block_map = None
-		if only_largest_block:
-			# do one first pass and determine largest blocks
-			f = open_possibly_gzipped(list_file, stack)
-			block_sizes = defaultdict(int)
-			logger.info('Reading %s to determine block sizes', list_file)
-			for line in f:
-				if line.startswith('#'):
-					continue
-				fields = line.split()
-				assert len(fields) == 4, 'Error parsing input file "{}"'.format(list_file)
-				readname, haplotype_name, phaseset, chromosome = fields
-				if haplotype_to_int[haplotype_name] != 0:
-					block_sizes[(chromosome,phaseset)] += 1
-			largest_block_map = dict()
-			chromosomes = set(chromosome for chromosome,phaseset in block_sizes.keys())
-			logger.info('Largest blocks per chromosome:')
-			for chromosome in sorted(chromosomes):
-				blocks = sorted(((size, phaseset) for (_chromosome,phaseset), size in block_sizes.items() if _chromosome == chromosome), reverse=True)
-				largest_block_size, largest_block = blocks[0]
-				largest_block_map[chromosome] = largest_block
-				logger.info('   %s: phaseset "%s" with %d tagged reads', chromosome, largest_block, largest_block_size)
-
-		# mapping of read names to haplotypes, 0 means untagged
-		haplotype = defaultdict(int)
-		tags_removed = 0
-		assigned_to_haplotype = 0
-		f = open_possibly_gzipped(list_file, stack)
-		logger.info('Reading %s', list_file)
-		for line in f:
-			if line.startswith('#'):
-				continue
-			fields = line.split()
-			assert len(fields) == 4, 'Error parsing input file "{}"'.format(list_file)
-			readname, haplotype_name, phaseset, chromosome = fields
-			haplotype_int = haplotype_to_int[haplotype_name]
-			if only_largest_block:
-				if (haplotype_int != 0) and (largest_block_map[chromosome] != phaseset):
-					tags_removed += 1
-					haplotype_int = 0
-			haplotype[readname] = haplotype_int
-			if haplotype_int != 0:
-				assigned_to_haplotype += 1
-		logger.info('... read %d records:', len(haplotype))
-		logger.info('...   %d reads are assigned to a haplotype.', assigned_to_haplotype)
-		if only_largest_block:
-			logger.info('...   %d reads have been unassigned because they are not from the largest block.', tags_removed)
+		readname_to_haplotype, known_reads = process_haplotag_list_file(
+			haplo_list,
+			line_parser,
+			only_largest_block,
+			discard_unknown_reads
+		)
 
 		output_h1_file = None
 		output_h2_file = None
@@ -231,7 +299,7 @@ def run_split(
 
 			for record in bamreader:
 				n_reads += 1
-				h = haplotype[record.query_name]
+				h = readname_to_haplotype[record.query_name]
 
 				if read_lengths_histogram is not None:
 					read_length = len(record.query_sequence)
@@ -258,7 +326,7 @@ def run_split(
 
 			for name, record in read_fastq(reads_file):
 				n_reads += 1
-				h = haplotype[name]
+				h = readname_to_haplotype[name]
 
 				if read_lengths_histogram is not None:
 					read_length = len(record[1].strip())
